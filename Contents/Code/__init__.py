2350d4bbb2c9cbdc79c8d6ce367e9d0e5ce17b87
ryan <ryan@Mac-Pro.local>
2011-03-29 01:57:17 -0400

    Upgraded to v2

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,23 +1,24 @@
-from PMS import Plugin, Log, DB, Thread, XML, HTTP, JSON, RSS, Utils
-from PMS.MediaXML import MediaContainer, DirectoryItem, PhotoItem
-
 PLUGIN_PREFIX   = "/photos/From10to300mm"
-RSS_FEED        = "http://from10to300mm.pixyblog.com/feed/entries/atom"
+RSS_FEED        = "http://from10to300mm.aminus3.com/feed/images/"
 
 ####################################################################################################
+
 def Start():
-  Plugin.AddRequestHandler(PLUGIN_PREFIX, HandlePhotosRequest, "From 10 to 300mm", "icon-default.jpg", "art-default.jpg")
-  Plugin.AddViewGroup("Pictures", viewMode="Pictures", contentType="photos")
-
+	Plugin.AddPrefixHandler(PLUGIN_PREFIX, HandlePhotosRequest, "From 10 to 300mm", "icon-default.jpg", "art-default.jpg")
+	Plugin.AddViewGroup("Pictures", viewMode="Pictures", mediaType="photos")
+	MediaContainer.art = R('art-default.jpg')
+	MediaContainer.viewGroup = 'Pictures'
+	
 ####################################################################################################
-def HandlePhotosRequest(pathNouns, count):
-  dir = MediaContainer('art-default.jpg', 'Pictures')
-  feed = RSS.Parse(RSS_FEED)
-  for item in feed.entries:
-    node = XML.ElementFromString(item.content[0].value, True)
-    summary = ' '.join(node.xpath("//text()")).replace('\n','').strip()
-    thumb = node.xpath("//img")[0].get('src')
-    src = thumb.replace('gen/500/','')
-    dir.AppendItem(PhotoItem(src, item.title, summary, thumb))
 
-  return dir.ToXML()
+def HandlePhotosRequest():
+	dir = MediaContainer()
+	feed = RSS.FeedFromURL(RSS_FEED)
+	for item in feed.entries:
+		title = item.title
+		node = HTML.ElementFromString(item.description)
+		thumb = node.xpath("//img")[0].get('src')
+		#src = thumb.replace('gen/500/','')
+		dir.Append(PhotoItem(thumb, title=title, thumb=thumb))
+
+	return dir
